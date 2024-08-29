15cdb993f1696dafc352a2a8241f00c2820c8067
val <walkbefaith@gmail.com>
2024-08-29 11:56:23 -0500

    carrusel

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,27 +1,55 @@
-import { useEffect } from "react"
-
+import { useEffect } from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import comiendoImage from '../assets/comiendo.png';
+import manasaImage from '../assets/manasa.png';
 
 export const Nosotros = () => {
-
   useEffect(() => {
-
     const clickear = () => {
       console.log("Click");
-    }
+    };
 
-    window.addEventListener("click", clickear)
+    window.addEventListener("click", clickear);
 
     return () => {
-      window.removeEventListener("click", clickear)
-    }
-
-  }, [])
-
+      window.removeEventListener("click", clickear);
+    };
+  }, []);
 
   return (
     <div className="container">
-        <h1 className="main-title">Nosotros</h1>
-        <p>Este es el componente "Nosotros"</p>
+      <h1 className="main-title">Nosotros</h1>
+      <div className="content-container">
+        <div className="carousel-container">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={comiendoImage}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={manasaImage}
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="text-container">
+          <p>Ofrecemos comida natural y pasteurizada para mascotas, elaborada con ingredientes de alta calidad, sin conservantes ni aditivos, diseñada para brindar una alimentación balanceada y saludable. Nuestro enfoque en la excelencia y perfección garantiza una nutrición óptima para perros de todas las edades y razas, llevándolos a vivir una vida más sana y feliz.</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
